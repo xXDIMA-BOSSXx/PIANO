b1beb8421ba74f5b5a3b397f8cf00a5ce690a840
xXDIMA-BOSSXx <dima.tschernobai@gmail.com>
2020-08-21 21:51:43 +0200

    Importing Project into GIt...

---
+++ PIANO/Assets/Scripts/Note.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    public AudioSource note;
+    public AudioSource tempAudio;
+    PlayerInput input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = FindObjectOfType<PlayerInput>();
+        note = GetComponentInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input.release)
+        {
+            if(GetComponentsInChildren<AudioSource>() != null)
+            foreach (AudioSource source in GetComponentsInChildren<AudioSource>())
+            {
+              source.volume -= Time.deltaTime * 1.65f;
+            }
+        }
+    }
+
+    public void PlayNote()
+    {
+        //PLay AUDIO
+        note.volume = 1f;
+        tempAudio = Instantiate (note, transform);
+        tempAudio.clip = note.clip;
+        tempAudio.Play();
+        Destroy(tempAudio.gameObject, tempAudio.clip.length);
+      
+    }
+}
---
+++ PIANO/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public bool playNote;
+    public bool release;
+   
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                hit.transform.GetComponent<Note>().PlayNote();
+                Debug.Log(hit.transform.name);
+            }
+        }
+
+        if( Input.GetMouseButtonUp(0))
+        {
+            release = true;
+
+        }else if (Input.GetMouseButtonDown(0))
+        {
+            release = false;
+        }
+        
+        
+       
+        
+        
+    }
+}
