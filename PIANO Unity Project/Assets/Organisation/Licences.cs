583ad5509e62534a241f9ce7c5399c6f8be95530
xXDIMA-BOSSXx <65425228+xXDIMA-BOSSXx@users.noreply.github.com>
2020-10-04 23:57:38 +0200

    importing assets. Trees, grass, water

---
+++ PIANO Unity Project/Assets/Organisation/Licences.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Licences : MonoBehaviour
+{
+  // <a href = 'https://www.freepik.com/photos/abstract' > Abstract photo created by starline - www.freepik.com</a>
+}
