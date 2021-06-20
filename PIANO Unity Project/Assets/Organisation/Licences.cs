1d61996282efbeb34df369059acff43abcc65bb1
xXDIMA-BOSSXx <65425228+xXDIMA-BOSSXx@users.noreply.github.com>
2021-06-20 20:42:39 +0200

    DELETING OLD PROJECT
    after a lot of bullshit, destroyed projects, different folders, month nand months of debugging........ I DELETE THIS DAMN THING

--- PIANO Unity Project/Assets/Organisation/Licences.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Licences : MonoBehaviour
-{
-  // <a href = 'https://www.freepik.com/photos/abstract' > Abstract photo created by starline - www.freepik.com</a>
-}
